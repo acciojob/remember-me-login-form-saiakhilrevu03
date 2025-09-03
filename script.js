f098327e75ac17749a92fae37a917a5f9b9f869f
saiakhilrevu03 <102013188+saiakhilrevu03@users.noreply.github.com>
2025-09-03 22:34:08 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,51 @@
 //your JS code here. If required.
+const form = document.getElementById("loginForm");
+const userNameInput = document.getElementById("username");
+const passwordInput = document.getElementById("password");
+const checkbox = document.getElementById("checkbox");
+const existingBtn = document.getElementById("existing");
+
+const savedUserName = localStorage.getItem("username");
+const savedPassword = localStorage.getItem("password");
+
+if(savedUserName && savedPassword){
+	existingBtn.style.display = "block";
+}
+
+form.addEventListener("submit",function(e){
+	e.preventDefault();
+
+	const username = userNameInput.value.trim();
+	const password = passwordInput.value.trim();
+
+	if(!username || !password) return;
+
+	alert("logged in as "+username);
+
+	
+	
+	if(checkbox.checked){
+		localStorage.setItem("username",username);
+		localStorage.setItem("password",password);
+		existingBtn.style.display="block";
+	}
+    else{
+		localStorage.remove("username");
+		localStorage.remove("password");
+		existingBtn.style.display="none";
+	}
+
+});
+existingBtn.addEventListener("click",function(){
+	const storedUser = localStorage.getItem("username");
+	if(storedUser){
+		alert("Logged in as "+storedUser);
+	}
+})
+
+
+
+
+
+
+
